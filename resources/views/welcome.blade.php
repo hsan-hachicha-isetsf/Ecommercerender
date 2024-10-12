e6ffcd15f87b9b6a9e21453f9eb04c67df90d671
hsan-hachicha-isetsf <hsan.hachicha@gmail.com>
2024-10-12 21:55:32 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon application Laravel</title>
+    <link href="{{ asset('frontend/assets/index-C8Fjzkvv.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div id="root"></div>
+    <script src="{{ asset('frontend/assets/index-CtYBjjRY.js') }}"></script>
+</body>
+</html>
